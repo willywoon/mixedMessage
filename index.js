ff0bba4e373493b3dfb7b702b34a9afb6403404f
willy <w.lunah@protonmail.com>
2021-09-23 05:28:50 -0300

    new branch

--- index.js
+++ index.js
@@ -1,8 +1,16 @@
 //global scope
 
-const message1 = ['Your sign is sun', 'Your sign is moon', 'Your sign is start', 'Your sign is fire'];
-const message2 = ['today You wil a nice day', 'today is a bad day for You'];
-const message3 = ['maybe today You have a good pratice', 'today is the day', ' You can do it'];
+//predeccion phrases
+
+const prediction1 = ['Your sign is sun', 'Your sign is moon', 'Your sign is start', 'Your sign is fire'];
+const prediction2 = ['today You wil a nice day', 'today is a bad day for You'];
+const prediction3 = ['maybe today You have a good pratice', 'today is the day', ' You can do it'];
+
+//joke phrases
+
+const joke1 = ['lolo', 'trololo', 'mujajahhaah'];
+const joke2 = ['not today', 'maybe not', 'maybe tomorrow', 'maybe later'];
+const joke3 = []
 
 //this functin return a number between the length of the array passed
 
@@ -12,6 +20,18 @@ const randomGenerator = (arr) => {
     return result;
 }
 
+// create a object with the posible random message
+
+const message = {
+    _message1: prediction1[randomGenerator(message1)],
+    _message2: prediction2[randomGenerator(message2)],
+    _message3: prediction3[randomGenerator(message3)],
+    newMessage() {
+
+        return `${this._message1}, ${this._message2} and ${this._messawge3}`;
+    }
+}
+
 //this function create a new random message and print the message
 
 const newMessage = () => {
@@ -19,9 +39,21 @@ const newMessage = () => {
     let phrase2 = message2[randomGenerator(message2)];
     let phrase3 = message3[randomGenerator(message3)];
 
-    console.log(`${phrase1}, ${phrase2} and ${phrase3}`);
+    return `${phrase1}, ${phrase2} and ${phrase3}`;
+}
+
+
+//create a object to organize the message for tematic
+
+const messages = {
+    _predection: [message1, message2, message3],
+    _joke: [joke1, joke2, joke3]
 }
 
 // call function
 
-newMessage();
+//newMessage();
+
+const newObject = message.newMessage();
+
+console.log(newObject)
